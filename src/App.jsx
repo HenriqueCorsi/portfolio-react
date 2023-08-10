ff698c90243e2f0f1cc3ca0ebf91d43b7f8fbb02
Paulo Henrique <ph55@hotmail.com>
2023-08-10 12:04:43 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import Projetos from './components/Projetos'
+import Sobre from './components/Sobre'
 import Title from './components/Title'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Title title="Projetos"/>
       <Projetos/>
+      <Sobre/>
     </>
   )
 }
