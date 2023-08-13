3c20ec2444215f888beee0e4e8f7eb0fc0a7c180
Paulo Henrique <ph55@hotmail.com>
2023-08-13 11:45:19 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
 import '../assets/css/footer-style.css'
 import iconEmail from '../assets/img/email-icon.png'
 
 function Footer(){
+    const [isCopied, setCopied] = useState();
+
+    const handleCopyClick = () => {
+        const emailElement = document.createElement('input');
+        emailElement.value = 'henrique.corsi11@gmail.com';
+        document.body.appendChild(emailElement);
+        emailElement.select();
+        document.execCommand('copy');
+        document.body.removeChild(emailElement);
+        setIsCopied(true);
+    }
+
+
     return(
         <footer>
            <div className='footer-icon'>
                 <img src={iconEmail} alt="icon email" />
            </div>
+           <h4>Entre em Contato</h4>
            <p>henrique.corsi11@gmail.com</p>
-           <button>Copiar Email</button>
+           <button onClick={handleCopyClick}>Copiar Email</button>
         </footer>
     )
 }
