03720b5cd6aa914d5c67eeec7f989d3276103f91
Paulo Henrique <ph55@hotmail.com>
2023-08-14 12:02:51 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,40 @@
-import { useState } from 'react';
-import '../assets/css/footer-style.css'
-import iconEmail from '../assets/img/email-icon.png'
+import React, { useState } from 'react';
+import '../assets/css/footer-style.css';
+import iconEmail from '../assets/img/email-icon.png';
 
-function Footer(){
-    const [isCopied, setCopied] = useState();
+function Footer() {
+  const [isCopied, setIsCopied] = useState(false);
 
-    const handleCopyClick = () => {
-        const emailElement = document.createElement('input');
-        emailElement.value = 'henrique.corsi11@gmail.com';
-        document.body.appendChild(emailElement);
-        emailElement.select();
-        document.execCommand('copy');
-        document.body.removeChild(emailElement);
-        setIsCopied(true);
-    }
+  const handleCopyClick = () => {
+    const emailElement = document.createElement('input');
+    emailElement.value = 'henrique.corsi11@gmail.com';
+    document.body.appendChild(emailElement);
+    emailElement.select();
+    document.execCommand('copy');
+    document.body.removeChild(emailElement);
+    setIsCopied(true);
 
+    // Remove o efeito de clique após um tempo para restaurar o botão ao estado normal
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1500);
+  };
 
-    return(
-        <footer>
-           <div className='footer-icon'>
-                <img src={iconEmail} alt="icon email" />
-           </div>
-           <h4>Entre em Contato</h4>
-           <p>henrique.corsi11@gmail.com</p>
-           <button onClick={handleCopyClick}>Copiar Email</button>
-        </footer>
-    )
+  return (
+    <footer>
+      <div className='footer-icon'>
+        <img src={iconEmail} alt="icon email" />
+      </div>
+      <h4>Entre em Contato</h4>
+      <p>henrique.corsi11@gmail.com</p>
+      <button
+        onClick={handleCopyClick}
+        className={isCopied ? 'copied' : ''}
+      >
+        {isCopied ? 'Email Copiado!' : 'Copiar Email'}
+      </button>
+    </footer>
+  );
 }
 
 export default Footer;
